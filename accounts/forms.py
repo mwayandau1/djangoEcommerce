91c472db8516ececdf2ceff8aa5f45ed4dd00d02
mwayandau1 <global-mosesayandau1@gmail.com>
2023-09-01 21:49:58 +0000

    Navigation

--- accounts/forms.py
+++
@@ -1,6 +0,0 @@
-from .models import User
-from django import forms
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'confirm_password']
